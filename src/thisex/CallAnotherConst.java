5c80a4b8b7affa1f46fa7473496f7645600fddcc
goforit615 <bupark82@gsm.hs.kr>
2025-02-22 16:21:43 +0900

    CallAnotherConst return this

--- src/thisex/CallAnotherConst.java
+++ src/thisex/CallAnotherConst.java
@@ -12,11 +12,19 @@ class Person {
         this.name = name;
         this.age = age;
     }
+
+    Person returnItSelf() {
+        return this;
+    }
 }
 public class CallAnotherConst {
     public static void main(String[] args) {
         Person noName = new Person();
         System.out.println(noName.name);
         System.out.println(noName.age);
+
+        Person p = noName.returnItSelf();   // this 값을 클래스 변수에 대입
+        System.out.println(p);  // noName.returnSelf()의 반환 값 출력
+        System.out.println(noName); // 참조 변수 출력
     }
 }
